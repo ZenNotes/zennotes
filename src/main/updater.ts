62e37e05b9d2952e2fb0b3c308cf77e1c821ab91
Adib Hanna <adibhanna@gmail.com>
2026-04-17 19:00:54 -0500

    Harden release note normalization

--- src/main/updater.ts
+++ src/main/updater.ts
@@ -36,11 +36,9 @@ function makeState(overrides: Partial<AppUpdateState> = {}): AppUpdateState {
 function decodeBasicHtmlEntities(value: string): string {
   return value
     .replace(/&nbsp;/gi, ' ')
-    .replace(/&amp;/gi, '&')
-    .replace(/&lt;/gi, '<')
-    .replace(/&gt;/gi, '>')
     .replace(/&quot;/gi, '"')
     .replace(/&#39;|&apos;/gi, "'")
+    .replace(/&amp;/gi, '&')
 }
 
 function normalizeReleaseNoteBody(body: string): string {
@@ -53,7 +51,7 @@ function normalizeReleaseNoteBody(body: string): string {
       .replace(/<\/?(ul|ol)\b[^>]*>/gi, '\n')
       .replace(/<\/?(p|div|section|article|header|footer|aside|blockquote|pre)\b[^>]*>/gi, '\n')
       .replace(/<\/?h[1-6]\b[^>]*>/gi, '\n')
-      .replace(/<[^>]+>/g, '')
+      .replace(/<\/?(strong|b|em|i|code|span)\b[^>]*>/gi, '')
   )
     .replace(/[ \t]+\n/g, '\n')
     .replace(/\n{3,}/g, '\n\n')
